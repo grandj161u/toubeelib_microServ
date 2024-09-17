045f1ab5d7dacfce7859a54e2ba49b9d39d99c51
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-09-17 16:46:26 +0200

    Ajout du repository des Rdv

---
+++ app/src/core/repositoryInterfaces/RdvRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace toubeelib\core\repositoryInterfaces;
+
+use toubeelib\core\domain\entities\rdv\Rdv;
+
+interface RdvRepositoryInterface
+{
+
+    public function save(Rdv $rdv): string;
+    public function getRdvById(string $id): Rdv;
+    public function getRdvByPraticienId(string $id): array;
+    public function getRdvByPatientId(string $id): array;
+}
