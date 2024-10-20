c67690333503ee2e78114ced7dacbcb1f901b212
Matheo_grdj <126459096+grandj161u@users.noreply.github.com>
2024-10-20 19:48:54 +0200

    modification de la classe initialement ModifierRdv en ModifierOuGererCycleRdv pour respecter l'api RESTful

---
+++ app/src/application/actions/ModifierOuGererCycleRdvAction.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace toubeelib\application\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use toubeelib\core\services\rdv\ServiceRdvInterface;
+use toubeelib\application\renderer\JsonRenderer;
+use toubeelib\core\services\rdv\ServiceRdvNotFoundException;
+use toubeelib\core\dto\ModifyRdvDTO;
+use toubeelib\core\dto\GererCycleRdvDTO;
+
+class ModifierOuGererCycleRdvAction extends AbstractAction {
+
+    protected ServiceRdvInterface $serviceRdv;
+
+    public function __construct(ServiceRdvInterface $serviceRdv) {
+        $this->serviceRdv = $serviceRdv;
+    }
+
+    public function __invoke (ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface {
+        $id = $args["id"];
+        $body = $rq->getParsedBody();
+
+        try {
+            if (isset($body['statut'])) {
+                $statut = $body["statut"];
+
+                $gererCycleRdv_DTO = new GererCycleRdvDTO($statut);
+
+                $rdv_DTO = $this->serviceRdv->GererCycleRdv($gererCycleRdv_DTO, $id);
+            }
+
+            if (isset($body['idSpecialite']) || isset($body['idPatient'])) {
+                $idSpecialite = $body["idSpecialite"] ?? null;
+                $idPatient = $body["idPatient"] ?? null;
+
+                $modify_RdvDTO = new ModifyRdvDTO($idPatient, $idSpecialite);
+
+                $rdv_DTO = $this->serviceRdv->modifierRdv($modify_RdvDTO, $id);
+            }
+        } catch (ServiceRdvNotFoundException $e) {
+           $data = [
+                'message' => $e->getMessage(),
+                'exception' => [
+                    'type' => get_class($e),
+                    'code' => $e->getCode(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ]
+            ];
+            return JsonRenderer::render($rs, 404, $data);
+        } catch (\Exception  $e) {
+            $data = [
+                'message' => $e->getMessage(),
+                'exception' => [
+                    'type' => get_class($e),
+                    'code' => $e->getCode(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ]
+            ];
+            return JsonRenderer::render($rs, 400, $data);
+        }
+
+        $data = [
+            'rdv' => $rdv_DTO,
+            'links' => [
+                'self' => [ 'href' => '/Rdvs/' . $id ], 
+                'modifier' => [ 'href' => '/Rdvs/' . $id ],
+                'annuler' => [ 'href' => '/Rdvs/' . $id ],
+            ]
+        ];
+
+        return JsonRenderer::render($rs, 200, $data);
+    }
+}
