8d8a849605343e5334184ddaef4b34a2f3042bec
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-01-08 16:43:33 +0100

    api gateway de départ initialisé

--- apiGateway/config/bootstrap.php
+++ apiGateway/config/bootstrap.php
@@ -4,7 +4,7 @@ use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 
 $builder = new ContainerBuilder();
-// $builder->addDefinitions(__DIR__ . '/settings.php');
+$builder->addDefinitions(__DIR__ . '/settings.php');
 // $builder->addDefinitions(__DIR__ . '/dependencies.php');
 
 $c = $builder->build();
---
+++ apiGateway/config/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+return  [
+
+    'displayErrorDetails' => true,
+    'logs.dir' => __DIR__ . '/../var/logs',
+];
