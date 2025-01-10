681c0341d6a2c7dc20a9efd674b919199f8a7493
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-01-10 15:29:03 +0100

    ajout gestion erreur dans praticenById

--- apiGateway/src/application/actions/GatewayPraticienByIdAction.php
+++ apiGateway/src/application/actions/GatewayPraticienByIdAction.php
@@ -1,35 +1,39 @@
 <?php
 
-// namespace gateway_tblb\application\actions;
+namespace gateway_tblb\application\actions;
 
-// use Psr\Http\Message\ResponseInterface;
-// use Psr\Http\Message\ServerRequestInterface;
-// use GuzzleHttp\Exception\ConnectException;
-// use GuzzleHttp\Exception\ClientException;
-// use GuzzleHttp\Exception\ServerException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpInternalServerErrorException;
 
-// class GatewayPraticienByIdAction extends AbstractGatewayAction
-// {
+class GatewayPraticienByIdAction extends AbstractGatewayAction
+{
 
-//     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
-//     {
-//         if (isset($args['idPraticien'])) {
-//             $idPraticien = $args['idPraticien'];
-//         } else {
-//             $idPraticien = null;
-//         }
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+        if (isset($args['idPraticien'])) {
+            $idPraticien = $args['idPraticien'];
+        } else {
+            $idPraticien = null;
+        }
 
-//         try {
-//             $response = $this->remote->request('GET', 'praticiens/' . $idPraticien);
-//         } catch (ConnectException | ServerException $e) {
-//             throw new InternalServiceErrorException(" … ");
-//         } catch (ClientException $e) {
-//             match ($e->getCode()) {
-//                 404 => throw new NotFoundServiceException(" ... "),
-//                 400 => throw new BadRequestException(" "
-//             };
-//         }
-
-//         return $response;
-//     }
-// }
+        try {
+            $response = $this->remote->request('GET', 'praticiens/' . $idPraticien);
+        } catch (ConnectException | ServerException $e) {
+            throw new HttpInternalServerErrorException($rq, "Internal server error");
+        } catch (ClientException $e) {
+            match ($e->getCode()) {
+                404 => throw new HttpNotFoundException($rq, "Ressource not found"),
+                403 => throw new HttpForbiddenException($rq, "Access forbidden"),
+                400 => throw new HttpBadRequestException($rq, "Bad request")
+            };
+        }
+        return $response;
+    }
+}
