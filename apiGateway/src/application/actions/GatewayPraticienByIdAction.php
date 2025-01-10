932be931c025e01884cecb0b09c6fd8e99459ccc
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-01-10 15:27:40 +0100

    fichier commenter pour pull

--- apiGateway/src/application/actions/GatewayPraticienByIdAction.php
+++ apiGateway/src/application/actions/GatewayPraticienByIdAction.php
@@ -1,27 +1,35 @@
 <?php
 
-namespace gateway_tblb\application\actions;
+// namespace gateway_tblb\application\actions;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+// use Psr\Http\Message\ResponseInterface;
+// use Psr\Http\Message\ServerRequestInterface;
+// use GuzzleHttp\Exception\ConnectException;
+// use GuzzleHttp\Exception\ClientException;
+// use GuzzleHttp\Exception\ServerException;
 
-class GatewayPraticienByIdAction extends AbstractGatewayAction
-{
+// class GatewayPraticienByIdAction extends AbstractGatewayAction
+// {
 
-    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
-    {
-        if (isset($args['idPraticien'])) {
-            $idPraticien = $args['idPraticien'];
-        } else {
-            $idPraticien = null;
-        }
+//     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+//     {
+//         if (isset($args['idPraticien'])) {
+//             $idPraticien = $args['idPraticien'];
+//         } else {
+//             $idPraticien = null;
+//         }
 
-        $response = $this->remote->request('GET', 'praticiens/' . $idPraticien);
-        // $body = $response->getBody()->getContents();
+//         try {
+//             $response = $this->remote->request('GET', 'praticiens/' . $idPraticien);
+//         } catch (ConnectException | ServerException $e) {
+//             throw new InternalServiceErrorException(" … ");
+//         } catch (ClientException $e) {
+//             match ($e->getCode()) {
+//                 404 => throw new NotFoundServiceException(" ... "),
+//                 400 => throw new BadRequestException(" "
+//             };
+//         }
 
-        // $rs->getBody()->write($body);
-        // return $rs->withHeader('Content-Type', 'application/json')
-        //     ->withStatus($response->getStatusCode());
-        return $response;
-    }
-}
+//         return $response;
+//     }
+// }
