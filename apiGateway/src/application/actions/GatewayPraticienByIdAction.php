346b3e348de0a91d33c2bb29835c56e45f386964
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-01-10 16:44:54 +0100

    Retrait de dépendances inutiles

--- apiGateway/src/application/actions/GatewayPraticienByIdAction.php
+++ apiGateway/src/application/actions/GatewayPraticienByIdAction.php
@@ -2,16 +2,6 @@
 
 namespace gateway_tblb\application\actions;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use GuzzleHttp\Exception\ConnectException;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ServerException;
-use Slim\Exception\HttpNotFoundException;
-use Slim\Exception\HttpForbiddenException;
-use Slim\Exception\HttpBadRequestException;
-use Slim\Exception\HttpInternalServerErrorException;
-
 class GatewayPraticienByIdAction extends GatewayGenericAction
 {
 
