b05cce2b0eae9b7d45c157bcd2c08d1ae0f56526
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-01-10 14:16:28 +0100

    Changement nom route API

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,17 +8,17 @@ return function( \Slim\App $app):\Slim\App {
 
     $app->get('/', \toubeelib\application\actions\HomeAction::class);
 
-    $app->get('/Rdvs/{id}', \toubeelib\application\actions\ConsulterRdvAction::class);
+    $app->get('/rdvs/{id}', \toubeelib\application\actions\ConsulterRdvAction::class);
 
-    $app->patch('/Rdvs/{id}', \toubeelib\application\actions\ModifierOuGererCycleRdvAction::class);
+    $app->patch('/rdvs/{id}', \toubeelib\application\actions\ModifierOuGererCycleRdvAction::class);
 
-    $app->get('/Rdvs/patient/{idPatient}', \toubeelib\application\actions\ConsulterRdvByPatientAction::class);
+    $app->get('/rdvs/patient/{idPatient}', \toubeelib\application\actions\ConsulterRdvByPatientAction::class);
 
-    $app->post('/Rdvs', \toubeelib\application\actions\CreerRdvAction::class);
+    $app->post('/rdvs', \toubeelib\application\actions\CreerRdvAction::class);
 
-    $app->post('/Praticiens', \toubeelib\application\actions\CreerPraticienAction::class);
+    $app->post('/praticiens', \toubeelib\application\actions\CreerPraticienAction::class);
 
-    $app->delete('/Rdvs/{id}', \toubeelib\application\actions\AnnulerRdvAction::class);
+    $app->delete('/rdvs/{id}', \toubeelib\application\actions\AnnulerRdvAction::class);
 
     // l'url s'utilise de cette manière : /DispoPraticien/{idPraticien}/2024-10-19/2024-10-20 et si vous voulez rajouter l'heure : /DispoPraticien/{idPraticien}/2024-10-19 16:00/2024-10-20 18:00
     $app->get('/DispoPraticien/{idPraticien}/{dateDebut}/{dateFin}', \toubeelib\application\actions\DispoByPraticienAction::class);
@@ -27,7 +27,7 @@ return function( \Slim\App $app):\Slim\App {
     $app->get('/PlanningPraticien/{idPraticien}/{dateDebut}/{dateFin}', \toubeelib\application\actions\PlanningPraticienAction::class);
 
     // l'url s'utilise de cette manière : /Praticiens?idPraticien=p1 ou /Praticiens
-    $app->get('/Praticiens', \toubeelib\application\actions\ListerOuRechercherPraticienAction::class);
+    $app->get('/praticiens', \toubeelib\application\actions\ListerOuRechercherPraticienAction::class);
 
     $app->post('/users/signin', \toubeelib\application\actions\SignInAction::class);
 
