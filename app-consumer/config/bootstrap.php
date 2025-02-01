4c936394a8eb4c8c554b0391c014cf0c192c3ac9
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-02-01 18:20:10 +0100

    ajout d'un bootstrap adapté pour récupérer le container

--- app-consumer/config/bootstrap.php
+++ app-consumer/config/bootstrap.php
@@ -1,28 +1,11 @@
 <?php
 
 use DI\ContainerBuilder;
-use Slim\Factory\AppFactory;
-use app_consumer\application\middlewares\Cors;
-use app_consumer\application\middlewares\GatewayAuthMiddleware;
 
 $builder = new ContainerBuilder();
 $builder->addDefinitions(__DIR__ . '/settings.php');
 $builder->addDefinitions(__DIR__ . '/dependencies.php');
 
 $c = $builder->build();
-$app = AppFactory::createFromContainer($c);
 
-$app->add(new Cors());
-// $app->add(GatewayAuthMiddleware::class);
-
-$app->addBodyParsingMiddleware();
-$app->addRoutingMiddleware();
-$app->addErrorMiddleware($c->get('displayErrorDetails'), false, false);
-
-//    ->getDefaultErrorHandler()
-//    ->forceContentType('application/json')
-
-$app = (require_once __DIR__ . '/routes.php')($app);
-$routeParser = $app->getRouteCollector()->getRouteParser();
-
-return $app;
+return $c;
